2d8a48d151b120a4f1d325b55e303b61ef8c6fc7
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2025-08-29 13:29:10 +0200

    build

--- web/src/main/kotlin/org/who/gdhcnvalidator/web/ServletInitializer.kt
+++ web/src/main/kotlin/org/who/gdhcnvalidator/web/ServletInitializer.kt
@@ -4,12 +4,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 class ServletInitializer : SpringBootServletInitializer() {
-	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-		return application.sources(
-			WebApplication::class.java,
-			JacksonConfig::class.java,
-			UIController::class.java,
-			RestController::class.java
-		)
-	}
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(
+            WebApplication::class.java,
+            JacksonConfig::class.java,
+            UIController::class.java,
+            GdhcnRestController::class.java   // renamed controller
+        )
+    }
 }
